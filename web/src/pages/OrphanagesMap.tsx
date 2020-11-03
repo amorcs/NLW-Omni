81a74bb767b8586f0a3ae03b52439885dbcc0d25
Marcos Sousa <marco.sds@hotmail.com>
2020-11-02 21:17:09 -0300

    projeto mobile foi criado

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,25 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 // import libriry
 import { Link } from "react-router-dom";
 import { FiArrowRight, FiPlus } from "react-icons/fi";
-import { Map, Popup } from 'react-leaflet';
+import { Map, Popup } from "react-leaflet";
 import { TileLayer, Marker } from "react-leaflet";
-import Leaflet from "leaflet";
+import mapIcon from "../utils/mapIcon";
 
-import 'leaflet/dist/leaflet.css';
+import "leaflet/dist/leaflet.css";
 // import internal
 import mapMarkerImg from "../images/map-marker.svg";
 
 // import css
-import '../styles/pages/orphanages-map.css';
+import "../styles/pages/orphanages-map.css";
+import api from "../services/api";
 
-const mapIcon = Leaflet.icon({
-  iconUrl: mapMarkerImg,
-  iconSize: [48,58],
-  iconAnchor: [24, 58],
-  popupAnchor: [170, 2]
-})
+interface Orphanage {
+  id: number;
+  latitude: number;
+  longitude: number;
+  name: string;
+}
+
 function OrphanagesMap() {
+  const [orphanages, setOrphanages] = useState<Orphanage[]>([]);
+  console.log(orphanages);
+  //rederiza apenas uma vez
+  useEffect(() => {
+    api.get("orphanages").then(response => {
+      setOrphanages(response.data);
+    });
+  }, []);
+
   return (
     <div id="page-map">
       <aside>
@@ -28,29 +39,43 @@ function OrphanagesMap() {
           <h2>Escolha um Orfanato no mapa</h2>
           <p>Muitas crianças estão esperando você</p>
         </header>
+
         <footer>
-            <strong>Belém</strong>
-            <span>Pará</span>
+          <strong>Belém</strong>
+          <span>Pará</span>
         </footer>
       </aside>
-      <Map className="map"
-        center={[-1.3769462,-48.4373472]}
+      <Map
+        className="map"
+        center={[-1.3769462, -48.4373472]}
         zoom={15}
-        style={{ width: '100%', height: '100%' }}>
-          <Marker
-            icon={mapIcon}
-            position={[-1.3769462,-48.4373472]}>
-            <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
-              Casa marcos
-              <Link to="">
-                <FiArrowRight size={20} color="#fff"/>
-              </Link>
-            </Popup>
-          </Marker>
-          <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        style={{ width: "100%", height: "100%" }}
+      >
+        <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        {orphanages.map(orphanage => {
+          return (
+            <Marker
+              icon={mapIcon}
+              position={[orphanage.latitude, orphanage.longitude]}
+              key={orphanage.id}
+            >
+              <Popup
+                closeButton={false}
+                minWidth={240}
+                maxWidth={240}
+                className="map-popup"
+              >
+                {orphanage.name}
+                <Link to={`/orphanages/${orphanage.id}`}>
+                  <FiArrowRight size={20} color="#fff" />
+                </Link>
+              </Popup>
+            </Marker>
+          );
+        })}
       </Map>
-      <Link to="/" className="create-orphanage">
-          <FiPlus size={32} color="#FFF"/>
+      <Link to="/orphanages/create" className="create-orphanage">
+        <FiPlus size={32} color="#FFF" />
       </Link>
     </div>
   );
